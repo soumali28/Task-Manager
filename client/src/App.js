473e28cd6355b9688bac259cc812fe278c67d7f6
Soumali Gorai <gorai.soumali@gmail.com>
2022-07-07 16:05:13 +0530

    signin form

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import ToDo from "./pages/ToDo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/" element={<ToDo />} />
+    </Routes>
   );
 }
 
---
+++ client/tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      zinc: {
+        400: "#a1a1aa",
+        500: "#71717a",
+        700: "#1f2937",
+        900: "#18181b",
+      },
+      purple: {
+        100: "#fae8ff",
+        200: "#f5d0fe",
+        300: "#f0abfc",
+        400: "#e879f9",
+        500: "#d946ef",
+        600: "#c026d3",
+        700: "#a21caf",
+        800: "#86198f",
+        900: "#701a75",
+      },
+      pink: {
+        600: "#db2777",
+        700: "#be185d",
+      },
+      rose: "#f43f5e",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
