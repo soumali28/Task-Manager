89072e964d801e161c603dc397adf22c9bf15053
Soumali Gorai <gorai.soumali@gmail.com>
2022-07-13 20:44:05 +0530

    first route

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -7,8 +8,8 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/client/src/pages/SignIn.jsx");
+app.get("/api/test", (req, res) => {
+    res.json({ message : "Hello World"})
 })
 
 app.listen(port, (err) => {
