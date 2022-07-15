00364913b19232bce1be67e91363b173acd49fa3
Soumali Gorai <gorai.soumali@gmail.com>
2022-07-15 19:47:42 +0530

    connecting the database with the app

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { errorHandler } = require("./server/middleware/errorMiddleware");
+const connectDb = require("./server/config/db");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+connectDb();
 
 // testing route
 app.get("/", (req, res) => {
@@ -14,11 +16,11 @@ app.get("/", (req, res) => {
 });
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/todos", require("./server/routes/todos"));
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, (err) => {
   if (err) return console.log(err);
---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDb = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Mongo db conected: ${conn.connection.host}`)
+    }
+    catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDb;
